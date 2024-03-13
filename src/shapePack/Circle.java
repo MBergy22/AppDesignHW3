3ff54a50e53181986bfc79550f2e33f680a86f4c
MBergy22 <icbinbergman@gmail.com>
2024-03-13 09:07:57 -0500

    Commit

---
+++ src/shapePack/Circle.java
@@ -0,0 +1,9 @@
+package shapePack;
+
+public class Circle extends Shape 
+{
+	public void printCircle() 
+	{
+		System.out.println("This is a circle, a subclass of Shape.");
+	}
+}
---
+++ src/shapeProcess/ShapeExecute.java
@@ -0,0 +1,23 @@
+package shapeProcess;
+
+import shapePack.SemiCircle;
+import shapePack.Square;
+
+public class ShapeExecute 
+{
+	public static void main(String[]args) 
+	{
+		Square square = new Square();
+		
+		square.printShape();
+		square.printSquare();
+		square.printRectangle();
+		
+		
+		SemiCircle semiCircle = new SemiCircle();
+		
+		semiCircle.printShape();
+		semiCircle.printSemiCircle();
+		semiCircle.printCircle();
+	}
+}
